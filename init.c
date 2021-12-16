a3114d66af41cde3da4ced41ad9e055725cca76c
massino <nacerddine.amassin@gmail.com>
2021-12-16 15:30:42 +0100

    jusque la tt va bien je vais tenter qlq chose

--- init.c
+++ init.c
@@ -1,6 +1,8 @@
 #include "init.h"
 #include "fonction.h"
 
+int taille_vecteur_Entree = 10;
+
 void init_n_conf(int tailleVecteur)
 {
     
@@ -9,10 +11,10 @@ void init_n_conf(int tailleVecteur)
 }
 
 s_Neurone init_neurone_vide(s_Neurone neurone){
-neurone.x=(double*)malloc(sizeof(double)*s_conf.n);
-neurone.w=(double*)malloc(sizeof(double)*s_conf.n);
+neurone.x=(double*)malloc(sizeof(double)*taille_vecteur_Entree);
+neurone.w=(double*)malloc(sizeof(double)*taille_vecteur_Entree);
 neurone.sortie=0;
-   for (int j=0;j<s_conf.n;j++)
+   for (int j=0;j<taille_vecteur_Entree;j++)
    {
    neurone.w[j]=0;
    neurone.x[j]=0;
@@ -29,14 +31,14 @@ s_Neurone init_neurone(double vecteurEntree[],fonctionActivation activation)
 s_Neurone neurone;
 neurone=init_neurone_vide(neurone);
 
-neurone.x=(double*)malloc(sizeof(double)*s_conf.n);
-neurone.w=(double*)malloc(sizeof(double)*s_conf.n);
+neurone.x=(double*)malloc(sizeof(double)*taille_vecteur_Entree);
+neurone.w=(double*)malloc(sizeof(double)*taille_vecteur_Entree);
 neurone.sortie=0.0;
 
 double somme=0.0;
-double result=0.0;
 
-neurone.w=init_rand_w(neurone.w,s_conf.n);
+
+neurone.w=init_rand_w(neurone.w,taille_vecteur_Entree);
 
 
 neurone.x=vecteurEntree;
@@ -44,7 +46,7 @@ neurone.x=vecteurEntree;
 
 
 //on somme les entrées 
-for (int i = 0 ; i < s_conf.n ; i++)
+for (int i = 0 ; i < taille_vecteur_Entree ; i++)
 {
 somme=somme+(neurone.w[i]*neurone.x[i]);
 }
@@ -54,28 +56,28 @@ somme=somme+(neurone.w[i]*neurone.x[i]);
 switch( activation )
 {
     case ReLU:
-        result=fRelu(somme);
+        neurone.sortie=fRelu(somme);
         break;
     case tangante:
-        result=tanhf(somme);
+        neurone.sortie=tanhf(somme);
         break;
     case sigmoide:
-        result=fsigmoid(somme);
+        neurone.sortie=fsigmoid(somme);
         break;
     case Identite:
-        result=fIdentite(somme);
+        neurone.sortie=fIdentite(somme);
         break;
     case Marche:
-        result=fMarche(somme);
+        neurone.sortie=fMarche(somme);
         break;
     default:
-       result=fRelu(somme);
+       neurone.sortie=fRelu(somme);
        break;
 
 }
 
  
-neurone.sortie=result;
+
 
     return neurone;
 }
@@ -116,10 +118,12 @@ perceptron.couches = malloc(perceptron.nb * sizeof *(perceptron.couches));
 int nb=2;
 //Notre premiére couche avec notre vecteur n'entre et 3 neurones 
 perceptron.couches[0]=init_layer(vecteurEntree,3);
+taille_vecteur_Entree=perceptron.couches[0].n;
  for (int x = 1; x < perceptron.nb; x++)
     {
         perceptron.couches[x]=init_layer(perceptron.couches[x-1].sortie,nb);
         nb--;
+        taille_vecteur_Entree=perceptron.couches[x].n;
        
     }
 
