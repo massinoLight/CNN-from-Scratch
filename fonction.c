c93f57c3917b1d364bf8b6319a927357f44fc9a7
massino <nacerddine.amassin@gmail.com>
2021-12-26 05:11:44 +0100

    backpro ajouté mais ne marche pas encore

--- fonction.c
+++ fonction.c
@@ -104,9 +104,38 @@ void affiche(int *tableau, int tailleTableau)
 /*********************************************************************************************************************************/
 
 
+// Backprop
+void backpropagation(int numInputs ,int numHiddenNodes ,int numOutputs){
 
 
+            double deltaOutput[numOutputs];
+            for (int j=0; j<numOutputs; j++) {
+                double errorOutput = (training_outputs[i][j]-outputLayer[j]);
+                deltaOutput[j] = errorOutput*dSigmoid(outputLayer[j]);
+            }
+
+            double deltaHidden[numHiddenNodes];
+            for (int j=0; j<numHiddenNodes; j++) {
+                double errorHidden = 0.0f;
+                for(int k=0; k<numOutputs; k++) {
+                    errorHidden+=deltaOutput[k]*outputWeights[j][k];
+                }
+                deltaHidden[j] = errorHidden*dSigmoid(hiddenLayer[j]);
+            }
+
+            for (int j=0; j<numOutputs; j++) {
+                outputLayerBias[j] += deltaOutput[j]*lr;
+                for (int k=0; k<numHiddenNodes; k++) {
+                    outputWeights[k][j]+=hiddenLayer[k]*deltaOutput[j]*lr;
+                }
+            }
+
+            for (int j=0; j<numHiddenNodes; j++) {
+                hiddenLayerBias[j] += deltaHidden[j]*lr;
+                for(int k=0; k<numInputs; k++) {
+                    hiddenWeights[k][j]+=training_inputs[i][k]*deltaHidden[j]*lr;
+                }
+            }
 
 
-
-
+}
